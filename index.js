e218f5f06fcfab0f32c054dc4f69dee02a5ed6df
MELLAL Fethi <fethimellal@gmail.com>
2021-08-05 02:23:02 +0200

    get ready to JQ

---
+++ index.js
@@ -0,0 +1,4 @@
+//just a reminder use JQ in editor if you are using jquery extension to show all the code list available for development
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  })
